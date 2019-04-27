36be5ee72969c4de5b88be778fc35f8d7b2853f9
Sammy Tang <samtang226@gmail.com>
2019-04-26 18:40:59 -0700

    init react src

---
+++ clicky-game/src/components/HelloBootstrap.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function HelloBootstrap() {
+  return (
+    <div className="container">
+      <div className="jumbotron">
+        <h1>Clicky Game!</h1>
+        <p>Click on an image to earn points, but don't click on any more than once!</p>
+      </div>
+      <div className="card mb-4">
+      
+      </div>
+    </div>
+  );
+}
+
+export default HelloBootstrap;
