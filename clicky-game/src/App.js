e75e0732ae4dc939e48749ed48410cf54f99ce37
Sammy Tang <samtang226@gmail.com>
2019-04-30 14:18:19 -0700

    fixed

--- clicky-game/src/App.js
+++ clicky-game/src/App.js
@@ -1,6 +1,6 @@
 //imports dependencies and files
 import React, { Component } from "react";
-import Jumbo from "./components/HelloBootstrap"
+import Jumbo from "./components/Jumbo"
 import Picture from "./components/Picture";
 import DBZ from "./DBZ.json";
 
@@ -9,67 +9,73 @@ class App extends Component {
   state = {
     DBZ,
     clickedDBZ: [],
-    score: 0
+    score: 0,
+    topScore: 0
   };
 
-//when you click on a card ... the DBZ is taken out of the array
+  //when you click on a card ... the DBZ is taken out of the array
   imageClick = event => {
     const currentDBZ = event.target.alt;
     const DBZAlreadyClicked =
       this.state.clickedDBZ.indexOf(currentDBZ) > -1;
 
-//if you click on a DBZ that has already been selected, the game is reset and cards reordered
+    //if you click on a DBZ that has already been selected, the game is reset and cards reordered
     if (DBZAlreadyClicked) {
       this.setState({
-        DBZ: this.state.DBZ.sort(function(a, b) {
+        DBZ: this.state.DBZ.sort(function (a, b) {
           return 0.5 - Math.random();
         }),
         clickedDBZ: [],
         score: 0
       });
 
-//if you click on an available DBZ, your score is increased and cards reordered
+      //if you click on an available DBZ, your score is increased and cards reordered
     } else {
-      this.setState(
-        {
-          DBZ: this.state.DBZ.sort(function(a, b) {
-            return 0.5 - Math.random();
-          }),
-          clickedDBZ: this.state.clickedDBZ.concat(
-            currentDBZ
-          ),
-          score: this.state.score + 1
-        },
-//if you get all 12 DBZ corrent you get a congrats message and the game resets        
-        () => {
-          if (this.state.score === 12) {
-            this.setState({
-              DBZ: this.state.DBZ.sort(function(a, b) {
-                return 0.5 - Math.random();
-              }),
-              clickedDBZ: [],
-              score: 0
-            });
+      if (this.state.score < this.state.topScore) {
+        this.setState(
+          {
+            DBZ: this.state.DBZ.sort(function (a, b) {
+              return 0.5 - Math.random();
+            }),
+            clickedDBZ: this.state.clickedDBZ.concat(
+              currentDBZ
+            ),
+            score: this.state.score + 1
           }
-        }
-      );
+        );
+      }
+      else {
+        this.setState(
+          {
+            DBZ: this.state.DBZ.sort(function (a, b) {
+              return 0.5 - Math.random();
+            }),
+            clickedDBZ: this.state.clickedDBZ.concat(
+              currentDBZ
+            ),
+            score: this.state.score + 1,
+            topScore: this.state.topScore + 1
+          }
+        );
+      }
     }
-  };
+  }
 
-//the order of components to be rendered: navbar, Jumbo, Picture, footer 
+  //the order of components to be rendered: navbar, Jumbo, Picture, footer 
   render() {
     return (
-      <div>
-        <Jumbo />
-        score={this.state.score}
-          {this.state.DBZ.map(DBZ => (
-            <Picture
-              imageClick={this.imageClick}
-              id={DBZ.id}
-              key={DBZ.id}
-              image={DBZ.image}
-            />
-          ))}
+      <div className="container">
+        <Jumbo
+          score={this.state.score}
+          topScore={this.state.topScore} />
+        {this.state.DBZ.map(DBZ => (
+          <Picture
+            imageClick={this.imageClick}
+            id={DBZ.id}
+            key={DBZ.id}
+            image={DBZ.image}
+          />
+        ))}
       </div>
     );
   }
--- clicky-game/src/components/Jumbo.js
+++ clicky-game/src/components/Jumbo.js
@@ -1,11 +1,13 @@
 import React from "react";
 
-function Jumbo() {
+function Jumbo(props) {
   return (
     <div className="container">
       <div className="jumbotron">
         <h1>Clicky Game!</h1>
         <p>Click on an image to earn points, but don't click on any more than once!</p>
+        <p>Score = {props.score}</p>
+        <p>Top Score = {props.topScore}</p>
       </div>
     </div>
   );
